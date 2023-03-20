8f031c25edb7c75792904da4ff1a84dadd32eb7a
bestemic <przemek.pawlik123@gmail.com>
2023-03-20 20:35:51 +0100

    Form with express using POST.

--- aplikacje_2/app.js
+++ aplikacje_2/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     fs.readFile('static/index.html', {encoding: 'utf-8'}, (err, data) => {
@@ -10,13 +12,14 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/send', (req, res) => {
-    res.send(`<h1>First name: ${req.query.fname}</h1><h2>Last name: ${req.query.lname}</h2><a href="/">Click here to return</a>`);
-});
+app.route('/send')
+    .get((req, res) => {
+        res.send(`<h1>First name: ${req.query.fname}</h1><h2>Last name: ${req.query.lname}</h2><a href="/">Click here to return</a>`);
+    })
+    .post((req, res) => {
+        res.send(`<h1>Age: ${req.body.age}</h1><a href="/">Click here to return</a>`);
+    });
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 });
-
-
-// zadanie to co wyżej ale za pomocą posta
