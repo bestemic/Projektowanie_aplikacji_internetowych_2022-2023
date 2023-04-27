206aa67178f753d86f44b223ba5328ae6b1c7924
bestemic <przemek.pawlik123@gmail.com>
2023-04-27 16:59:29 +0200

    Cleaned unused files

--- projekt_1/routes/auctions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const auctionsController = require('../controllers/auctionsController')
-
-router.get('/', auctionsController.getAuctions);
-
-module.exports = router;
